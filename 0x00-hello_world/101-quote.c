d623880e7492bf6ad53bf241ebc2ea525c91f402
mimibogecho <nelsonbogecho2014@gmail.com>
2023-06-15 13:53:28 -0700

    unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	writee (2,
+		"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+
+	Return (1);
+}
