522f66156b75bf9e6591534dd2979440e3227bed
mimibogecho <nelsonbogecho2014@gmail.com>
2023-06-16 04:44:46 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - Prints exactly as required to the standard error
  *
  * Return: 1
  */
-
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
