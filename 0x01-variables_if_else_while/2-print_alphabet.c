77884e0d0aa863362eb9fb344cfd5a3a46f9e8ba
mimibogecho <nelsonbogecho2014@gmail.com>
2023-06-17 05:30:23 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,22 +2,22 @@
 
 /**
  * main - Program that prints the alphabet in lowercase, followed by a new line
+ *
  * Return: 0
  */
 
 int main(void)
 
 {
-	char c;
+	int c;
 
 	c = 'a';
 	while
 	(c < 'z')
 {
 	putchar(c);
-	+= 1;
+	c += 1;
 }
-
 putchar('\n');
 
 return (0);
