d3105fd5e4056b1313e1315aaa9ff8d2adb5f48a
mimibogecho <nelsonbogecho2014@gmail.com>
2023-06-17 12:18:35 -0700

    print combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints all possible combinations of single digitnumbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = '0';
+
+	while (a <= '9')
+	{
+		putchar(a);
+	if (a != '9')
+
+	{
+		putchar(',');
+
+	}
+	++a;
+
+	}
+putchar('\n');
+return (0);
+
+}
