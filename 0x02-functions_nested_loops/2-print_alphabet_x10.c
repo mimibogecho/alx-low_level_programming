7c5a1a818fd76ecbd1ec094e3b9ea73204e1b085
mimibogecho <nelsonbogecho2014@gmail.com>
2023-06-25 06:57:25 -0700

    2 print alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - printing in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int d;
+
+	d = 0;
+while (d < 10)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	
+	d++;
+	}
+
+}
+
+
+
+
+
