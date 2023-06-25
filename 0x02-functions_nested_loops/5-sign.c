6cfe3d4bbefd6520745bd6bc6e10f097c03bf478
mimibogecho <nelsonbogecho2014@gmail.com>
2023-06-25 11:24:21 -0700

    5 sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints sig of a number
  * @n: The number to be checked
- * Return: 1 for  positive num, -1 for negative num, or 0 for anything else
+ * Return: 1 for  positive num, -1 for negative num or zero for anything else
  */
 
 int print_sign(int n)
@@ -13,7 +13,7 @@ int print_sign(int n)
 		_putchar(43);
 		return (1);
 	}
-	else if (n > 0)
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
