589ae59a11de21cc238d48021a257462f27be2e5
mimibogecho <nelsonbogecho2014@gmail.com>
2023-06-26 09:00:56 -0700

    reseting int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Resets and takes the pointer to updated int value 98
+ * @n: Integer input pointer
+ * Return:
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - program that prints _putchar
+ *
+ * return: 1
+ */
+
+int _putchar(int c)
+{
+	return(write(1, &c, 1));
