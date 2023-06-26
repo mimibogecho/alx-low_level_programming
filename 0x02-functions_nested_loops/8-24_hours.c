51981edada2d3d41d1a2cbcec3fcbefd52d65760
mimibogecho <nelsonbogecho2014@gmail.com>
2023-06-26 02:30:57 -0700

    print time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
 /**
- * jack_bauer - The function that print minutes in a day
+ * jack_bauer - Prints every minute of the day of Jack Bauer
  * Return: ...
  */
 
 void jack_bauer(void)
 {
 
-	int a;
-	int b;
-	int c;
-	int d;
+	int a, b, c, d;
 
 	for (a = 0; a <= 2; a++)
 	{
