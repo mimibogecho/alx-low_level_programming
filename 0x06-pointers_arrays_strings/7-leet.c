f347bbdee097a0e8d61417b661bd6c9456bc77d3
mimibogecho <nelsonbogecho2014@gmail.com>
2023-06-30 12:12:31 -0700

    encodes astring

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,15 +2,14 @@
 
 /**
  * leet - Encodes a string into 1337
- * @m: input value
+ * @m: Input value
  * Return: m
  */
 
 char *leet(char *m)
 {
 	int b, c;
-
-	char s1[] = "aAAeEoOtTlL";
+	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
 	for (b = 0; m[b] != '\0'; b++)
