297860c3e5c82ce3ecdd6ef96a43040a43ed6cad
mimibogecho <nelsonbogecho2014@gmail.com>
2023-06-30 12:02:27 -0700

    encodes astring

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337
+ * @m: input value
+ * Return: m
+ */
+
+char *leet(char *m)
+{
+	int b, c;
+
+	char s1[] = "aAAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (b = 0; m[b] != '\0'; b++)
+	{
+		for (c = 0; c < 10; c++)
+		{
+		if (m[b] == s1[c])
+		{
+			m[b] = s2[c];
+		}
+		}
+	}
+	return (m);
+}
